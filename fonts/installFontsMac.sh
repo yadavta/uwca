01bacc7da286f932b08be24ab39d32e3a9734655
yadavta <tanushyadav@gmail.com>
2023-01-30 00:43:49 -0800

    remove comments to enable copying into terminal

--- fonts/installFontsMac.sh
+++ fonts/installFontsMac.sh
@@ -1,18 +1,14 @@
 cd ~/Library/Fonts && {
 	printf "\n *** Starting Font Installation *** \n"
 	printf "\n *** Downloading Fonts *** \n"
-	# Get font files from internet
 	curl -s -S -0 'https://homes.cs.washington.edu/~tanush/uwca/clear-sans.zip' --output clear-sans.zip
 	curl -s -S -0 'https://homes.cs.washington.edu/~tanush/uwca/hk-grotesk.zip' --output hk-grotesk.zip
 	
-	# Unzip fonts
 	printf "\n *** Unzipping Fonts *** \n"
 	unzip -q clear-sans
 	unzip -q hk-grotesk
 	
 	printf "\n *** Sanitizing Files *** \n"
-	# Remove duplicate copies
-	# This section could definetly be better written
 	rm -rf clear-sans.zip
 	rm -rf clear-sans
 	rm -rf hk-grotesk.zip
